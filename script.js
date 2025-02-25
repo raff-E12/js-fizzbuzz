2ca367f8ef65f3346e67e710b7b9b0e22fa52275
Raff_12 <raffelecoppola381@gmail.com>
2025-02-25 19:04:38 +0100

    feat: Rappresentazione concettuale in codice

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 // Scrittura di un programma che stampi i numeri da 0 a 100
+//Dove i relativi numeri divisibili per 3 o per 5 rappresentano la frase 'Frizz-Buzz'
 
-let Index = 0;
-let 
+// Indice esppresso in variabile
+let Index = 1;
+
+console.log('Inzio Loop:');
+
+//il tipo di ciclo con il tipo di condizione da applicare in seguito.
+// Ogni condizione verifica se alcuni numeri sono divisibili per 3 o per 5,
+// è stampano dei valori espressi nelle seguenti condizioni nel loop che avviene.
+
+for (Index; Index <= 100; Index++) {
+    if (Index % 3 === 0 && Index % 5 === 0) {
+        console.log('Fizz-Buzz');
+    } else if(Index % 3 === 0){
+        console.log('Fizz');
+    } else if(Index % 5 === 0){
+        console.log('Buzz')
+    } else{
+        console.log(Index);
+    }
+}
+
+// Fine del loop, dopo la representazione dei relativi risultati.
+console.log('Fine Loop, Grazie mille per aver visto.');
--- script_ex.js
+++
@@ -1,9 +0,0 @@
-//Avere un risultato che deve stampare dei numeri pari.
-
-// let Number = 1;
-// let Num_2 = 2;
-
-let Nume_op = (12 * 40) + 30;
-console.log(Nume_op);
-
-let bottone_1 = document.getElementById('hello').onclick(()=>{ funzione()})
